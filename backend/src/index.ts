064815ccccc3f560bc906335264f278caaaed22d
Carlos Alexandre <alexfilho062005@gmail.com>
2025-01-14 17:04:19 -0300

    :spakles feat:  integrating front to back cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from 'cors'
 import rotasProdutos from "./routes/produtos";
 
 const app = express();
 const porta = 4000;
 
+app.use(cors());
 app.use("/produtos", rotasProdutos);
 
 
