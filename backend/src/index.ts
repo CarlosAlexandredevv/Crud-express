094256b9b356182271d0fdf3ed4bc6e266bbf860
Carlos Alexandre <alexfilho062005@gmail.com>
2025-01-14 16:24:00 -0300

    :sparkles: feat: router get products

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import rotasProdutos from "./routes/produtos";
+
 const app = express();
 const porta = 4000;
+
+app.use("/produtos", rotasProdutos);
 
 
 app.listen(porta, () => {
